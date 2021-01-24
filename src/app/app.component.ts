0d70f91a6312296d2320dc56d0df933dd6e6c056
Daniel Figueira <danfig87@gmail.com>
2021-01-24 12:18:18 +0000

    Finished mobile faq list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'faq-accordion-card';
+
+  faqs = [
+    {
+      question: 'How many team members can I invite?',
+      answer: 'Iusto accusata per et, ex idque iisque meliore usu. Mea iudico soleat fastidii eu.',
+      selected: false
+    },
+    {
+      question: 'What is the maximum file upload size?',
+      answer: 'No more than 2GB. All files in your account must fit your allotted storage space.',
+      selected: true
+    },
+    {
+      question: 'How do I reset my password?',
+      answer: 'Sea duis legendos partiendo in. Vix ne etiam nemore epicurei. Delenit dolorum repudiare est cu.',
+      selected: false
+    },
+    {
+      question: 'Can I cancel my subscription?',
+      answer: 'Doctus delicata neglegentur ea per, sumo docendi ut vis, has ne regione definiebas.',
+      selected: false
+    },
+    {
+      question: 'Do you provide additional support?',
+      answer: 'Quo no tibique detraxit posidonium, eu usu atomorum pericula.',
+      selected: false
+    }];
+
+  onFaqIconClicked(faq: any): void {
+    this.faqs.forEach((entry) => entry.selected = false);
+    faq.selected = true;
+  }
 }
